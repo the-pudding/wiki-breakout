b125f6cd9bf747df5f79fb52451dcb2bb678ac35
russellgoldenberg <russellgoldenberg@gmail.com>
2018-10-18 12:03:32 -0400

    update dev version

--- src/js/audio.js
+++ src/js/audio.js
@@ -65,7 +65,7 @@ function playBg(article) {
 	if (track && !track.playing()) {
 		track.volume(0);
 		track.play();
-		track.fade(0, 0.25, FADE_DUR * 4);
+		track.fade(0, 0.4, FADE_DUR * 4);
 	}
 }
 
